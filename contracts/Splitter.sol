e82eedfcd7c9b3658bb471d2795af67429c925a9
Serhii R <SRozdorozhnii@luxoft.com>
2018-02-23 18:19:09 +0200

    refactor

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -6,13 +6,16 @@ contract Splitter {
         uint funded;
         uint withdrawed;
     }
-    address reciever1;
-    address reciever2;
-    address sender;
-    mapping(address => BalanceInfo) balances;
+    event FundsWithdrawed(address, uint);
+    address public reciever1;
+    address public reciever2;
+    address public owner;
+    mapping(address => BalanceInfo) public balances;
 
-    function Splitter(address rec1, address rec2) public payable {
-        sender = msg.sender;
+    function Splitter(address rec1, address rec2) public {
+        require(rec1 != 0);
+        require(rec2 != 0);
+        owner = msg.sender;
         balances[rec1] = BalanceInfo(0,0);
         balances[rec2] = BalanceInfo(0,0);
         reciever1 = rec1;
@@ -20,7 +23,7 @@ contract Splitter {
     }
     
     modifier onlySender() {
-        require(msg.sender == sender);
+        require(msg.sender == owner);
         _;
     }
 
@@ -29,26 +32,40 @@ contract Splitter {
         _;
     }
 
+    modifier fundsAvailable() {
+        bool available_balance = balances[msg.sender].funded >= balances[msg.sender].withdrawed;
+        require(available_balance);
+        _;
+    }
+
     function split() public onlySender payable {
         balances[reciever1].funded += msg.value/2;
         balances[reciever2].funded += msg.value/2;
     }
+    
 
-    function withdraw() public onlyReciever {
-        uint change = balances[msg.sender].funded - balances[msg.sender].withdrawed;
-        if(change <=0) revert();
-        if(!msg.sender.send(change)){
-            revert();
-        }
-        balances[msg.sender].withdrawed += change;
+
+    function withdraw() public onlyReciever fundsAvailable {
+        uint diff = getBal(msg.sender);
+        balances[msg.sender].withdrawed += diff; 
+        msg.sender.transfer(diff);
+        FundsWithdrawed(msg.sender, diff);
     }
 
-    function getBalance() public returns (uint256) {
-        return this.balance;
+    function getBal(address addr) internal view returns (uint256){
+        return balances[addr].funded - balances[addr].withdrawed;
     }
 
-    function getAvailableBalance() public onlyReciever returns (uint256) {
-        return balances[msg.sender].funded - balances[msg.sender].withdrawed;
+    function getAvailableBalance() public onlyReciever fundsAvailable view returns (uint256) {
+        
+        return getBal(msg.sender);
+    }
+    
+    function collectDust() public onlySender {
+        if(balances[reciever1].withdrawed == balances[reciever1].funded &&
+        balances[reciever2].withdrawed == balances[reciever2].funded
+        )
+        msg.sender.transfer(this.balance);
     }
 
 }
--- test/splitter.js
+++ test/splitter.js
@@ -1,104 +1,92 @@
 var Splitter = artifacts.require("Splitter");
-
+const expectedExceptionPromise = require("./expected_exception_testRPC_and_geth.js");
 
 contract('Splitter', function(accounts) {
-
-
-  it("should have 0 balance initially", function() {
-    return Splitter.deployed().then(function(instance) {
-      return instance.getBalance.call();
-    }).then(function(balance) {
-      assert.equal(balance.valueOf(), 0, "shouldn't have 0 balance initially");
-    });
-  });
-
-
-  it("should split evenly", function() {
-    return Splitter.deployed().then(function(instance) {
-      return instance.split.sendTransaction({from:accounts[0], value:1e18}).then(function() {
-      	return new Promise(function(resolve, reject) {
-      		resolve(instance);
-      	});
-      });
-    }).then(function(instance) {
-	  return instance.getAvailableBalance.call({from:accounts[1]});
-    }).then(function(balance) {
-    	assert.equal(balance.valueOf(), 5e17, "should have 5e17 balance");
-    });
-  });
-
-
-  it("should withdraw", function() {
-    return Splitter.deployed().then(function(instance) {
-      return instance.split.sendTransaction({from:accounts[0], value:1e18}).then(function() {
-        return new Promise(function(resolve, reject) {
-          resolve(instance);
+    var instance;
+    beforeEach(function() {
+        return Splitter.new(accounts[1], accounts[2]).then(function(contract) {
+            instance = contract;
         });
-      });
-    }).then(function(instance) {
-    return instance.withdraw.sendTransaction({from:accounts[1]}).then(
-      function() {
-        return new Promise(function(resolve, reject) {
-          resolve(instance);
-        })
-      });
-    }).then(function(instance) {
-      return instance.getAvailableBalance.call({from:accounts[1]});
-    }).then(function(balance) {
-      assert.equal(balance.valueOf(), 0, "should have 0 balance");
     });
-  });
 
-
-  it("should not withdraw to creator", function() {
-    return Splitter.deployed().then(function(instance) {
-      return instance.split.sendTransaction({from:accounts[0], value:1e18}).then(function() {
+    it("should have 0 balance initially", function() {
         return new Promise(function(resolve, reject) {
-          resolve(instance);
+            resolve(web3.eth.getBalance(instance.address));
+        }).then(function(balance) {
+            assert.equal(balance.valueOf(), 0, "shouldn't have 0 balance initially");
         });
-      });
-    }).then(function(instance) {
-    return instance.withdraw.sendTransaction({from:accounts[0]}).then(
-      function() {
-      },
-      function() {
-        return new Promise(function(resolve, reject) {
-          resolve(instance);
-        })
-      });
-    }).then(function(instance) {
-      return instance.getAvailableBalance.call({from:accounts[1]});
-    }).then(function(balance) {
-      assert.equal(balance.valueOf(), 5e17, "should have 5e17 balance");
     });
-  });
 
-
-  it("should have 0 balance after everyone withdrawed", function() {
-    return Splitter.deployed().then(function(instance) {
-      return instance.split.sendTransaction({from:accounts[0], value:1e18}).then(function() {
-        return new Promise(function(resolve, reject) {
-          resolve(instance);
+    it("should split evenly", function() {
+        return instance.split({
+            from: accounts[0],
+            value: 1e18
+        }).then(function() {
+            return instance.getAvailableBalance.call({
+                from: accounts[1]
+            });
+        }).then(function(balance) {
+            assert.equal(balance.valueOf(), 5e17, "should have 5e17 balance");
         });
-      });
-    }).then(function(instance) {
-    return instance.withdraw.sendTransaction({from:accounts[1]}).then(
-      function() {
-        return new Promise(function(resolve, reject) {
-          resolve(instance);
-        })
-      });
-    }).then(function(instance) {
-    return instance.withdraw.sendTransaction({from:accounts[2]}).then(
-      function() {
-        return new Promise(function(resolve, reject) {
-          resolve(instance);
-        })
-      });
-    }).then(function(instance) {
-      return instance.getBalance.call();
-    }).then(function(balance) {
-      assert.equal(balance.valueOf(), 0, "should have 0 balance");
     });
-  });
+
+    it("should withdraw", function() {
+        return instance.split.sendTransaction({
+            from: accounts[0],
+            value: 1e18
+        }).then(function() {
+            return instance.withdraw.sendTransaction({
+                from: accounts[1]
+            }).then(function() {
+                return instance.getAvailableBalance.call({
+                    from: accounts[1]
+                });
+            }).then(function(balance) {
+                assert.equal(balance.valueOf(), 0, "should have 0 balance");
+            });
+        });
+    });
+
+
+    it("should not withdraw to creator", function() {
+        return instance.split.sendTransaction({
+            from: accounts[0],
+            value: 1e18
+        }).then(function() {
+            return expectedExceptionPromise(function() {
+                    return instance.withdraw.sendTransaction({
+                        from: accounts[0]
+                    });
+                })
+                .then(function() {
+                    return instance.getAvailableBalance.call({
+                        from: accounts[1]
+                    });
+                }).then(function(balance) {
+                    assert.equal(balance.valueOf(), 5e17, "should have 5e17 balance");
+                });
+        });
+
+    });
+
+    it("should have 0 balance after everyone withdrawed", function() {
+        return instance.split({
+            from: accounts[0],
+            value: 1e18
+        }).then(function() {
+            return instance.withdraw.sendTransaction({
+                from: accounts[1]
+            }).then(function() {
+                return instance.withdraw.sendTransaction({
+                    from: accounts[2]
+                }).then(function() {
+                    return new Promise(function(resolve, reject) {
+                        resolve(web3.eth.getBalance(instance.address));
+                    })
+                }).then(function(balance) {
+                    assert.equal(balance.valueOf(), 0, "should have 0 balance");
+                });
+            });
+        });
+    });
 });
